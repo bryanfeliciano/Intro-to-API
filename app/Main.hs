3c2070dd5d7bc84628bd540005c874e9fd88b4b9
bryan <bryanfeliciano892@gmail.com>
2022-05-28 12:51:41 -0500

    creating translaterequest data type created

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,18 @@
 module Main where
 
+import Network.Wreq
+import Data.Text (Text)
+
 import Lib
+
+data TranslateRequest = TranslateRequest {
+    q:: Text,
+    source:: Text,
+    target:: Text,
+    format:: Text
+} deriving (Show)
 
 main :: IO ()
 main = do 
-    print "works"
+    rsp <- post "https://translate.argospentech.com/translate" 
+    print rsp
